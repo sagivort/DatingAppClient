0a94883f0254f12f3fa9168859a2356902d7ef70
Sagiv Tuvia <Sagiv@Sagiv.com>
2025-09-18 14:55:43 +0300

    Remove  imports

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,30 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal, OnInit, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
+
+  // ctor -- 1
+  constructor() {}
+
+  // Dependency Injection -- 2
+  private http = inject(HttpClient);
+
+  // -- 3
+  ngOnInit(): void {
+    this.http.get('http://localhost:5094/api/members').subscribe({
+      next: members => console.log(members),
+      error: err => console.log(err),
+      complete: () => console.log('Request completed')
+  });
+  }
+
   protected readonly title = signal('DatingApp-Client');
+  protected myname = 'John Doe';
+  
 }
